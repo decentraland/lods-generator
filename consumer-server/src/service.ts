9b49f9796d50a8e366f5ca3856b269d4ab6868a2
Alejo Thomas Ortega <alejo.ortega@decentraland.org>
2024-01-15 14:05:45 -0300

    feat: add first version of consumer server with backbone (#4)
    * feat: add first version of consumer server with backbone and SQS integration

---
+++ consumer-server/src/service.ts
@@ -0,0 +1,20 @@
+import { Lifecycle } from '@well-known-components/interfaces'
+import { AppComponents, GlobalContext, TestComponents } from './types'
+import { setupRouter } from './controllers/routes'
+
+export async function main(program: Lifecycle.EntryPointParameters<AppComponents | TestComponents>) {
+  const { components, startComponents } = program
+  const globalContext: GlobalContext = {
+    components
+  }
+
+  const router = await setupRouter(globalContext)
+  // register routes middleware
+  components.server.use(router.middleware())
+  // register not implemented/method not allowed/cors responses middleware
+  components.server.use(router.allowedMethods())
+  // set the context to be passed to the handlers
+  components.server.setContext(globalContext)
+
+  await startComponents()
+}
---
+++ consumer-server/src/utils/aws-config.ts
@@ -0,0 +1,29 @@
+import { AppComponents, AwsConfig } from '../types'
+
+export async function buildAWSConfiguration({
+  config,
+  logs
+}: Pick<AppComponents, 'config' | 'logs'>): Promise<AwsConfig | undefined> {
+  const logger = logs.getLogger('aws-config')
+  const region = await config.getString('AWS_REGION')
+  const accessKeyId = await config.getString('AWS_ACCESS_KEY_ID')
+  const secretAccessKey = await config.getString('AWS_SECRET_ACCESS_KEY')
+  const sqsUrl = await config.getString('AWS_QUEUE_URL')
+
+  let awsConfig: AwsConfig | undefined = undefined
+
+  if (!!region && !!accessKeyId && !!secretAccessKey && !!sqsUrl) {
+    awsConfig = {
+      region,
+      credentials: {
+        accessKeyId,
+        secretAccessKey
+      },
+      sqsUrl
+    }
+  } else {
+    logger.info('Could not load AWS Configuration')
+  }
+
+  return awsConfig
+}
