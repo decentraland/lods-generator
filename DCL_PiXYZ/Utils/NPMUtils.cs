68cde71ac0b711a13d6ce8d8f8810bebe6e50046
Juan Molteni <juanignaciomolteni@gmail.com>
2024-01-16 07:10:32 -0300

    Working version, requires cleaning

--- DCL_PiXYZ/Utils/NPMUtils.cs
+++ DCL_PiXYZ/Utils/NPMUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace DCL_PiXYZ.Utils
 {
@@ -56,41 +57,62 @@ namespace DCL_PiXYZ.Utils
             }
         }
         
-        public static string RunNPMToolAndReturnExceptionIfPresent(string sceneManifestProjectDirectory, string coords)
+        public static async Task<(string, bool)> RunNPMToolAndReturnExceptionIfPresent(string sceneManifestProjectDirectory, string coords, int timeoutInMilliseconds)
         {
+            bool isSDK7 = false;
+            bool readTimeout = false;
+            string exception = "";
             // Set up the process start information
             ProcessStartInfo install = new ProcessStartInfo
             {
-                FileName = "powershell", // or the full path to npm if not in PATH
-                Arguments = "npm run start --coords=" + coords, // replace with your npm command
+                FileName = "powershell",
+                Arguments = "npm run start --coords=" + coords, 
                 WorkingDirectory = sceneManifestProjectDirectory,
                 RedirectStandardError = true,
-                RedirectStandardOutput = true, // if you want to read output
+                RedirectStandardOutput = true, 
                 UseShellExecute = false,
                 CreateNoWindow = true
             };
-
             
             // Start the process
             using (Process process = Process.Start(install))
             {
-                
-                // Read the output (if needed)
-                using (StreamReader reader = process.StandardOutput)
-                {
-                    string result = reader.ReadToEnd();
-                }
-                
                 //TODO: Not sure I have to wait after printing output for the process to end. Thats why it has this weird place
-                process.WaitForExit(); 
+                if (process.WaitForExit(timeoutInMilliseconds))
+                {
+                    //using (StreamReader reader = process.StandardOutput)
+                    //{
+                        //string result = reader.ReadToEnd();
+                        //Console.WriteLine(result);
+                        //isSDK7 = !result.Contains("sdk7? false");
+                    //}
+                    
+                    // Read the output (if needed)
+                    using (StreamReader reader = process.StandardError)
+                    {
+                        Task<string> readTask = reader.ReadLineAsync();
+                        if (await Task.WhenAny(readTask, Task.Delay(500)) == readTask)
+                            exception = readTask.Result;
+                        else
+                            readTimeout = true;
+                    }
+                    
+                    return (exception, readTimeout);
+                }
 
-                // Read the output (if needed)
+                process.Kill();
+
                 using (StreamReader reader = process.StandardError)
-                    return reader.ReadLine();
-                
-
+                {
+                    Task<string> readTask = reader.ReadLineAsync();
+                    if (await Task.WhenAny(readTask, Task.Delay(2000)) == readTask)
+                        exception = readTask.Result;
+                    else
+                        readTimeout = true;
+                }
+                    
+                return (exception, readTimeout);
             }
-            return "";
         }
 
         public static void RunNPMTool(string sceneManifestProjectDirectory, string coords)
