dc714f3ad238ab9c1192c724a9c8261bcb70b4ff
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-01-26 12:41:04 -0300

    feat: message handler v0 (#19)

---
+++ consumer-server/src/adapters/memory-queue.ts
@@ -0,0 +1,28 @@
+import { AsyncQueue } from '@well-known-components/pushable-channel'
+import { AppComponents, QueueService } from '../types'
+
+export function createMemoryQueueAdapter({ logs }: Pick<AppComponents, 'logs'>): QueueService {
+  const logger = logs.getLogger('memory-queue')
+  const queue = new AsyncQueue((action) => void 0)
+
+  logger.info('Initializing memory queue adapter')
+
+  async function send(message: any) {
+    await queue.enqueue(message)
+  }
+
+  async function receiveSingleMessage() {
+    const message = (await queue.next()).value
+    return message ? [message] : []
+  }
+
+  async function deleteMessage() {
+    // noop
+  }
+
+  return {
+    send,
+    receiveSingleMessage,
+    deleteMessage
+  }
+}
