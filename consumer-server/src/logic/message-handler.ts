620cb5390eff58237b94a280ee363216d940e882
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-02-05 10:53:22 -0300

    fix: message handling (#26)

--- consumer-server/src/logic/message-handler.ts
+++ consumer-server/src/logic/message-handler.ts
@@ -8,24 +8,22 @@ export function createMessageHandlerComponent({
   const logger = logs.getLogger('message-handler')
 
   async function handle(message: any): Promise<void> {
-    console.log({ message })
-    logger.info('Handling', { type: message.type, id: message.entity.entityId })
-
     if (message.entity.entityType !== 'scene') {
-        return
+      return
     }
-    logger.info('Handling message', { message })
 
+    logger.debug('Processing scene deployment', { message })
     const entityId = message.entity.entityId
     const base = message.entity.metadata.scene.base
 
-    try {
-      const filesToUpload = await lodGenerator.generate(entityId, base)
-      logger.info('LODs correctly generated', { files: filesToUpload.join(', '), entityId })
-    //   await storage.storeFiles(filesToUpload, base, parsedMessage.entity.entityTimestamp.toString())
-    } catch (error: any) {
-      logger.error('Failed while generating LODs', { error, entityId })
-    }
+    // try {
+    const filesToUpload = await lodGenerator.generate(entityId, base)
+    logger.info('LODs correctly generated', { files: filesToUpload.join(', '), entityId })
+    await storage.storeFiles(filesToUpload, base, message.entity.entityTimestamp.toString())
+    // } catch (error: any) {
+    //   // TODO: dlq/retry queue
+    //   throw error
+    // }
   }
 
   return { handle }
