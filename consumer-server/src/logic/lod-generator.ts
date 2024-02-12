065f239e6296a8d991999a16e26c70184320ea62
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-02-12 13:24:27 -0300

    Added suggestion

--- consumer-server/src/logic/lod-generator.ts
+++ consumer-server/src/logic/lod-generator.ts
@@ -8,9 +8,9 @@ import { LodGeneratorComponent } from '../types'
 export function createLodGeneratorComponent(): LodGeneratorComponent {
   const projectRoot = path.resolve(__dirname, '..', '..', '..') // project root according to Dockerfile bundling
   const lodGeneratorProgram = path.join(projectRoot, 'api', 'DCL_PiXYZ.exe') // path to the lod generator program
+  const outputPath = path.join(os.tmpdir(), "built-lods")
 
   async function generate(entityId: string, basePointer: string): Promise<string[]> {
-    const outputPath = path.join(os.tmpdir(), "built-lods")
 
     if (!fs.existsSync(outputPath)) {
       fs.mkdirSync(outputPath, { recursive: true })
