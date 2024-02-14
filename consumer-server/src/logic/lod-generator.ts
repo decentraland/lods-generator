c76915d694608862c4e2369b3c2aed618007472a
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-02-14 10:21:48 -0300

    fix: debugging log (#33)

--- consumer-server/src/logic/lod-generator.ts
+++ consumer-server/src/logic/lod-generator.ts
@@ -3,14 +3,15 @@ import path from 'path'
 import os from 'os'
 import fs from 'fs'
 
-import { LodGeneratorComponent } from '../types'
+import { AppComponents, LodGeneratorComponent } from '../types'
 
-export function createLodGeneratorComponent(): LodGeneratorComponent {
+export function createLodGeneratorComponent({ logs }: Pick<AppComponents, 'logs'>): LodGeneratorComponent {
+  const logger = logs.getLogger('lod-generator')
   const projectRoot = path.resolve(__dirname, '..', '..', '..') // project root according to Dockerfile bundling
   const lodGeneratorProgram = path.join(projectRoot, 'api', 'DCL_PiXYZ.exe') // path to the lod generator program
   const outputPath = path.join(os.tmpdir(), "built-lods")
 
-  async function generate(entityId: string, basePointer: string): Promise<string[]> {
+  async function generate(basePointer: string): Promise<string[]> {
 
     if (!fs.existsSync(outputPath)) {
       fs.mkdirSync(outputPath, { recursive: true })
@@ -20,7 +21,7 @@ export function createLodGeneratorComponent(): LodGeneratorComponent {
     const files: string[] = await new Promise((resolve, reject) => {
       exec(commandToExecute, (error, _stdout, stderr) => {
         const debugFiles = fs.readdirSync(outputPath)
-        console.log('debugFiles', debugFiles)
+        logger.debug('Read files', { debugFiles: debugFiles.join(', ')})
 
         const generatedFiles = fs.readdirSync(`${outputPath}/${basePointer}`)
         // if files exists return otherwise reject
