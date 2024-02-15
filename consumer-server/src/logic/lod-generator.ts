1b2b995a89a5b2ad709409d8cbba3c4ea781a004
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-02-15 17:02:05 -0300

    feat: enhance lod-generator error logs (#39)

--- consumer-server/src/logic/lod-generator.ts
+++ consumer-server/src/logic/lod-generator.ts
@@ -3,13 +3,14 @@ import path from 'path'
 import os from 'os'
 import fs from 'fs'
 
-import { LodGeneratorComponent } from '../types'
+import { AppComponents, LodGeneratorComponent } from '../types'
 
-export function createLodGeneratorComponent(): LodGeneratorComponent {
+export function createLodGeneratorComponent({ logs }: Pick<AppComponents, 'logs'>): LodGeneratorComponent {
   const projectRoot = path.resolve(__dirname, '..', '..', '..') // project root according to Dockerfile bundling
   const lodGeneratorProgram = path.join(projectRoot, 'api', 'DCL_PiXYZ.exe') // path to the lod generator program
   const sceneLodEntitiesManifestBuilder = path.join(projectRoot, 'scene-lod') // path to the scene lod entities manifest builder
   const outputPath = path.join(os.tmpdir(), 'built-lods')
+  const logger = logs.getLogger('lod-generator')
 
   async function generate(basePointer: string): Promise<string[]> {
     let files: string[] = []
@@ -19,11 +20,19 @@ export function createLodGeneratorComponent(): LodGeneratorComponent {
 
     const commandToExecute = `${lodGeneratorProgram} "coords" "${basePointer}" "${outputPath}" "${sceneLodEntitiesManifestBuilder}"`
     files = await new Promise((resolve, _) => {
-      exec(commandToExecute, (_error, _stdout, _stderr) => {
+      exec(commandToExecute, (error, _stdout, stderr) => {
+        if (error || stderr) {
+          logger.error('Failed to generate LODs', { error: error?.message || 'Something failed' })
+          const parsedStderr = (stderr as string) || ''
+          const lines = parsedStderr.split('\n')
+          lines.forEach((line) => {
+            logger.debug('Error line', { error: line })
+          })
+        }
+
         const processOutput = `${outputPath}/${basePointer}`
         if (fs.existsSync(processOutput)) {
           const generatedFiles = fs.readdirSync(processOutput)
-          // if files exists return otherwise reject
           resolve(generatedFiles.map((file) => `${processOutput}/${file}`))
         } else {
           resolve([])
