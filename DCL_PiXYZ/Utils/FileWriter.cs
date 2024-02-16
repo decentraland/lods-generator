a81c2e0e2753b434905b431dc12b16928edd406a
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-02-16 14:59:51 -0300

    Added file writer class (#42)

---
+++ DCL_PiXYZ/Utils/FileWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace DCL_PiXYZ.Utils
+{
+    public class FileWriter
+    {
+        public static void WriteToFile(string message, string fileName)
+        {
+            using (StreamWriter file = new StreamWriter(fileName, true))
+                file.WriteLine(message);
+        }
+    }
+}
