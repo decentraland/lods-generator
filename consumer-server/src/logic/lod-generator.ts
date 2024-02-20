487e69270245daa69b8a220d067ffdf3d2af5f7a
Alejo Thomas Ortega <alejothomasortega@gmail.com>
2024-02-20 12:46:34 -0300

    chore: improve consumer-server logs about .exe execution (#43)

--- consumer-server/src/logic/lod-generator.ts
+++ consumer-server/src/logic/lod-generator.ts
@@ -2,44 +2,55 @@ import { exec } from 'child_process'
 import path from 'path'
 import fs from 'fs'
 
-import { AppComponents, LodGeneratorComponent } from '../types'
+import { LodGenerationResult, LodGeneratorComponent } from '../types'
 
-export function createLodGeneratorComponent({ logs }: Pick<AppComponents, 'logs'>): LodGeneratorComponent {
+export function createLodGeneratorComponent(): LodGeneratorComponent {
   const projectRoot = path.resolve(__dirname, '..', '..', '..') // project root according to Dockerfile bundling
   const lodGeneratorProgram = path.join(projectRoot, 'api', 'DCL_PiXYZ.exe') // path to the lod generator program
   const sceneLodEntitiesManifestBuilder = path.join(projectRoot, 'scene-lod') // path to the scene lod entities manifest builder
   const outputPath = path.join(projectRoot, 'built-lods')
-  const logger = logs.getLogger('lod-generator')
 
-  async function generate(basePointer: string): Promise<string[]> {
-    let files: string[] = []
-    if (!fs.existsSync(outputPath)) {
-      fs.mkdirSync(outputPath, { recursive: true })
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true })
+  }
+
+  async function generate(basePointer: string): Promise<LodGenerationResult> {
+    let result: LodGenerationResult = {
+      error: undefined,
+      lodsFiles: [],
+      logFile: ''
     }
 
     const commandToExecute = `${lodGeneratorProgram} "coords" "${basePointer}" "${outputPath}" "${sceneLodEntitiesManifestBuilder}"`
-    files = await new Promise((resolve, _) => {
-      exec(commandToExecute, (error, _stdout, stderr) => {
+    result = await new Promise((resolve, _) => {
+      exec(commandToExecute, (error, _, stderr) => {
         if (error || stderr) {
-          logger.error('Failed to generate LODs', { error: error?.message || 'Something failed' })
-          const parsedStderr = (stderr as string) || ''
-          const lines = parsedStderr.split('\n')
-          lines.forEach((line) => {
-            logger.debug('Error line', { error: line })
-          })
+          result.error = {
+            message: error?.message || 'Unexpected error',
+            detailedError: ((stderr as string) || '').replace('\n', ' ')
+          }
         }
 
         const processOutput = `${outputPath}/${basePointer}`
         if (fs.existsSync(processOutput)) {
           const generatedFiles = fs.readdirSync(processOutput)
-          resolve(generatedFiles.map((file) => `${processOutput}/${file}`))
+          result = generatedFiles.reduce((acc, file) => {
+            if (file.endsWith('output.txt')) {
+              acc.logFile = `${processOutput}/${file}`
+            } else {
+              acc.lodsFiles.push(`${processOutput}/${file}`)
+            }
+            return acc
+          }, result)
+
+          resolve(result)
         } else {
-          resolve([])
+          resolve(result)
         }
       })
     })
 
-    return files
+    return result
   }
 
   return { generate }
